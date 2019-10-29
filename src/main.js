d898c2c5683bea2834ab4bceba9f0ce672490d10
atallah-salah <atallah_salah@outlook.com>
2019-10-30 00:13:17 +0200

    add jquery

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 
+//  Import bootstrap 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 Vue.config.productionTip = false
 
 new Vue({
